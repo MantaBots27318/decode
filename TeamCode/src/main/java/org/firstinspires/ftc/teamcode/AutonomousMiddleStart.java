278a604515a45470e4b48baf2228dce8df717787
Martin Dutang <martin.dutang@gmail.com>
2025-11-15 18:47:15 -0500

    changed gradle to go back to old version of roadrunner. worked on autonomous opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousMiddleStart.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousMiddleStart.java
@@ -35,6 +35,8 @@ package org.firstinspires.ftc.teamcode;
 
 
 
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
@@ -66,61 +68,116 @@ import org.firstinspires.ftc.teamcode.vision.Vision;
  *   below the name of the Limelight on the top level configuration screen.
  */
 
-@Autonomous (name = "Sensor: Limelight3A")
+@Autonomous
 public class AutonomousMiddleStart extends LinearOpMode {
 
+
+    enum ActionMode {
+        MOVE_TO_BALL,
+        MOVE_TO_BASKET,
+        ACTION3,
+
+    }
+
+    ActionMode mState = ActionMode.MOVE_TO_BALL ;
+
+    ActionMode mActionMode;
     Vision          mVision;
     MecanumDrive    mDrive;
     Collecting      mCollecting;
 
+
     @Override
     public void runOpMode() throws InterruptedException {
 
-
+        mState = ActionMode.MOVE_TO_BALL;
 
         telemetry.setMsTransmissionInterval(11);
         mCollecting = new Collecting();
         mVision = new Vision(telemetry);
         mVision.initialize(hardwareMap);
 
+
+        double y_pos_pattern = 20;
+        double x_pos_GPP_pattern =25 ;
+        double x_pos_PGP_pattern =60 ;
+        double x_pos_PPG_pattern =84 ;
+        double x_pos_shooting_position = 96;
+        double y_pos_shooting_position =24;
+
         Pose2d beginPose = new Pose2d(0, 0, 0);
         mDrive = new MecanumDrive(hardwareMap, beginPose);
 
         telemetry.addData(">", "Robot Ready.  Press Play.");
+        telemetry.update();
+        Action move_to_ballGPP = mDrive.actionBuilder(beginPose)
+//                .splineTo(new Vector2d(x_pos_GPP_pattern, y_pos_pattern), Math.PI / 2)
+//                .lineToX(20)
+                .build();
+
+        Action move_to_ballPGP = mDrive.actionBuilder(beginPose)
+                .splineTo(new Vector2d(x_pos_PGP_pattern , y_pos_pattern), Math.PI / 2)
+                .build();
+        Action move_to_ballPPG = mDrive.actionBuilder(beginPose)
+                .splineTo(new Vector2d(x_pos_PPG_pattern, y_pos_pattern), Math.PI / 2)
+                .build();
+
+        Action move_to_basket = mDrive.actionBuilder(beginPose)
+                .waitSeconds(10)
+                .splineTo(new Vector2d(x_pos_shooting_position, y_pos_shooting_position), Math.PI / 4)
+                .build();
+        Vision.Pattern pattern = mVision.readPattern();
+        telemetry.addLine("Pattern is " + pattern.text());
         telemetry.update();
 
         waitForStart();
 
         while (opModeIsActive()) {
 
-            Vision.Pattern pattern = mVision.readPattern();
-            telemetry.addLine("Pattern is " + pattern.text());
-            if (mVision.readPattern() == Vision.Pattern.GPP){
-                Actions.runBlocking(
-                        mDrive.actionBuilder(beginPose)
-                                .splineTo(new Vector2d(36,30), Math.PI/2)
-                                .build());
-            }
-            if (mVision.readPattern() == Vision.Pattern.PGP){
-                Actions.runBlocking(
-                mDrive.actionBuilder(beginPose)
-                        .splineTo(new Vector2d(60,30), Math.PI/2)
-                        .build());
-            }
-            if (mVision.readPattern() == Vision.Pattern.PPG){
-                Actions.runBlocking(
-                        mDrive.actionBuilder(beginPose)
-                                .splineTo(new Vector2d(84,30), Math.PI/2)
-                                .build());
-            }
-            mCollecting.intake();
-            Actions.runBlocking(
-                    mDrive.actionBuilder(beginPose)
-                            .splineTo(new Vector2d(135,48), Math.PI/4)
-                            .build());
-            mCollecting.shooting();
-        }
+            if(mState == ActionMode.MOVE_TO_BALL) {
+                 pattern = mVision.readPattern();
+                telemetry.addLine("Pattern is " + pattern.text());
+                telemetry.update();
+                if (mVision.readPattern() == Vision.Pattern.GPP) {
+//                    move_to_ballGPP.run(new TelemetryPacket());
+//                    telemetry.addLine(" Doing the GPP pattern" );
+                    Actions.runBlocking(
+                            mDrive.actionBuilder(beginPose)
+                                    .splineTo(new Vector2d(x_pos_GPP_pattern , y_pos_pattern), Math.PI / 2)
 
+                                    .build());
+
+                    if (!move_to_ballGPP.run(new TelemetryPacket()) ){
+                        mState = ActionMode.MOVE_TO_BASKET ;
+                    }
+                }
+
+                if (mVision.readPattern() == Vision.Pattern.PGP) {
+                    move_to_ballPGP.run(new TelemetryPacket());
+                    telemetry.addLine(" Doing the PGP pattern" );
+                    if (!move_to_ballPGP.run(new TelemetryPacket()) ){
+                        mState = ActionMode.MOVE_TO_BASKET ;
+                    }
+                }
+
+                if (mVision.readPattern() == Vision.Pattern.PPG) {
+                    move_to_ballPPG.run(new TelemetryPacket() );
+                    telemetry.addLine(" Doing the PPG pattern" );
+                    if (!move_to_ballPPG.run(new TelemetryPacket()) ){
+                        mState = ActionMode.MOVE_TO_BASKET ;
+                    }
+                }
+            }
+
+            if (mState == ActionMode.MOVE_TO_BASKET ){
+
+                mCollecting.intake();
+                move_to_basket.run(new TelemetryPacket());
+            mCollecting.shooting();
+            }
+//
+        }
+        telemetry.update();
         mVision.close();
 
 
