fb36da3a0fbe14d4a0b55a6c5658f5acda49066f
nlemperiere <nlemperiere@fasny.org>
2025-11-15 19:16:18 -0500

    Adding limelight field configuration map

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/configurations/ConfLimelight.java
@@ -0,0 +1,53 @@
+/* -------------------------------------------------------
+   Copyright (c) [2025] FASNY
+   All rights reserved
+   -------------------------------------------------------
+   Servos configuration data
+   ------------------------------------------------------- */
+
+package org.firstinspires.ftc.teamcode.configurations;
+
+/* System includes */
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class ConfLimelight {
+
+    // To select if the servo shall be mocked --- not yet activated
+    private       boolean                mShallMock   = false;
+
+    // Mapping between limelight name on the hub and the motor direction
+    private       String                 mName;
+    // Reference servo positions for the given servo (or servo couple)
+    private final Map<String, Integer>   mPipelines   = new LinkedHashMap<>();
+
+    public ConfLimelight(String Name)
+    {
+        mName = Name;
+        mShallMock    = false;
+    }
+
+    public ConfLimelight(ConfLimelight Configuration)
+    {
+        mShallMock = Configuration.mShallMock;
+        mName = Configuration.mName;
+        for (Map.Entry<String, Integer> pipeline : Configuration.mPipelines.entrySet()) {
+            mPipelines.put(pipeline.getKey(),pipeline.getValue());
+        }
+    }
+
+    public void addHw(String Name) { mName = Name;  }
+    public void addPipeline(String Name, Integer Id)   { mPipelines.put(Name, Id); }
+
+    public String                     getHw()                  { return mName;}
+    public boolean                    shallMock()              { return mShallMock; }
+    public Map<String, Integer>       getPipelines()           { return mPipelines; }
+
+    public Integer                    getPipeline(String Name) {
+        if(mPipelines.containsKey(Name)) {
+            return mPipelines.get(Name);
+        }
+        return -1;
+    }
+
+}
